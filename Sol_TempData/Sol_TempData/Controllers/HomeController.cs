7f8edff722d413ac2aec6634a69f2d97c4043b00
Kishor Naik <kishor.naik011.net@gmail.com>
2019-05-16 21:10:11 +0530

    Code Commit

---
+++ Sol_TempData/Sol_TempData/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_TempData.Models;
+
+namespace Sol_TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.TempData["Data"] = "Kishor";
+
+            //return View();
+            return RedirectToAction("Privacy");
+        }
+
+        public IActionResult Privacy()
+        {
+            var data = this.TempData["Data"] as String;
+
+            //return View();
+            return RedirectToAction("Index", "RedirectNewHome");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sol_TempData/Sol_TempData/Controllers/RedirectNewHomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_TempData.Controllers
+{
+    public class RedirectNewHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.Data = this.TempData["Data"] as String;
+
+            return View();
+        }
+    }
+}
